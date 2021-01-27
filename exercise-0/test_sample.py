20144fcf730d22fcfe90a81a867779e3ceb9eec8
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-01-27 15:16:59 +0000

    Second code

--- exercise-0/test_sample.py
+++ exercise-0/test_sample.py
@@ -1,6 +1,17 @@
-def func(x):
-    return x + 1
+def add_one(n):
+    return n + 1
 
+def add_five(n):
+    return n + 5
 
-def test_answer():
-    assert func(3) == 5
+def double(n):
+    return n * n
+
+def test_add_one():
+    assert add_one(11) == 10
+
+def test_add_five():
+    assert add_five(11) == 16
+
+def test_double():
+    assert double(11) == 22
